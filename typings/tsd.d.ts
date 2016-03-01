cd866c623b0bffc01c73393f2be8e3055f651771
nhatphan1988 <thamtudethuong@gmail.com>
2016-03-01 21:58:12 +0800

    remove build

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="requirejs/require.d.ts" />
 /// <reference path="googlemaps/google.maps.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
